ccc0b68240f43b86525c64a1b94e6351d9d9fd45
sayildiz <y.samet97@gmail.com>
2022-01-04 02:42:58 +0100

    project init

---
+++ src/main/java/com/sayildiz/kbe_AppService/KbeAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sayildiz.kbe_AppService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayildiz/kbe_AppService/KbeAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayildiz.kbe_AppService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
