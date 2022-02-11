c47a5df2891843a74346d5461da77dc37e62d764
sayildiz <y.samet97@gmail.com>
2022-02-11 04:43:34 +0100

    feat: CurrencyAPI is now Scheduled Daily to consume for the USDRates

--- src/main/java/com/sayildiz/kbe_AppService/external_currency_api/CurrencyServiceImpl.java
+++ src/main/java/com/sayildiz/kbe_AppService/external_currency_api/CurrencyServiceImpl.java
@@ -1,18 +1,33 @@
 package com.sayildiz.kbe_AppService.external_currency_api;
 
+import com.sayildiz.kbe_AppService.file_transfer.Sftp_Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CurrencyServiceImpl implements CurrencyService{
+
     @Autowired
     private RestTemplate restTemplate;
+
+    private CurrencyExchangeRateUSD todaysRate;
+    private final Logger logger = LoggerFactory.getLogger(CurrencyServiceImpl.class);
 
     public final String url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json";
     @Override
     public CurrencyExchangeRateUSD getExchangeRateEURtoUSD() {
-        return restTemplate.getForObject(
-                url, CurrencyExchangeRateUSD.class);
+        return todaysRate;
+    }
+
+    @Scheduled(fixedDelay = 24, timeUnit = TimeUnit.HOURS)
+    public void getExchangeRateEveryDay() {
+        todaysRate = restTemplate.getForObject(url, CurrencyExchangeRateUSD.class);
+        logger.info("Todays USDRate =" + todaysRate.getUsd());
     }
 }
